43164f3c64e5837d6fd49ff85308219292ff5845
Michael Hudson <mnkotagu@gmail.com>
2023-03-09 17:08:38 +0300

    Implement 100-my_calculator.py source file

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+if __name__ == "__main__":
+    args_c = len(sys.argv) - 1
+    if args_c != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operators = ["+", "-", "*", "/"]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    op = sys.argv[2]
+    
+    if op not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+    else:
+        if op == "+":
+            result = add(a, b)
+        elif op == "-":
+            result = sub(a, b)
+        elif op == "*":
+            result = mul(a, b)
+        elif op == "/":
+            result = div(a, b)
+        print("{} {} {} = {}".format(a, op, b, result))
