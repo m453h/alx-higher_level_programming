e3220438e786dcb2934f8ff40caf5ab5a598601c
Michael Hudson <mnkotagu@gmail.com>
2023-03-09 15:54:25 +0300

    Implement 2-args.py source file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    args_c = len(sys.argv) - 1
+    if args_c == 0:
+        print("{} arguments.".format(0))
+    else:
+        if args_c == 1:
+            print("{} argument:".format(args_c))
+        else:
+            print("{} arguments:".format(args_c))
+        for i in range (1, args_c + 1):
+            print(sys.argv[i])
