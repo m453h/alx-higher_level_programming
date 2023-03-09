4d98337202a5635cfce7f8674ad67e0d80bbea64
Michael Hudson <mnkotagu@gmail.com>
2023-03-09 13:22:27 +0300

    Implement 1-calculation.py source file

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,16 +3,16 @@ from calculator_1 import *
 if __name__ == "__main__":
     a = 10
     b = 5
-    operations  = ["+", "-", "*", "/"]
+    operations = ["+", "-", "*", "/"]
 
     for op in operations:
-    	if op == "+":
-    		result = add(a, b)
-    	elif op == "-":
-    		result = sub(a, b)
-    	elif op == "*":
-    		result = mul(a, b)
-    	elif op == "/":
-    		result = div(a, b)
+        if op == "+":
+            result = add(a, b)
+        elif op == "-":
+            result = sub(a, b)
+        elif op == "*":
+            result = mul(a, b)
+        elif op == "/":
+            result = div(a, b)
 
-    	print("{} {} {} = {}".format(a, op, b, result))
+        print("{} {} {} = {}".format(a, op, b, result))
