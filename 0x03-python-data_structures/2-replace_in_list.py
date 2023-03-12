39513cd8b2464ff5cf2569588203f94004201b31
Michael Hudson <mnkotagu@gmail.com>
2023-03-12 13:52:10 +0300

    Implement 2-replace_in_list.py source file

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    my_list_len = len(my_list)
+
+    if idx >= my_list_len:
+        return my_list
+
+    my_list[idx] = element
+
+    return my_list
