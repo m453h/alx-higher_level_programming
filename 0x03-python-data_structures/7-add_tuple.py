3431cd6a7cc24801fda45cf738890a49310f2d5d
Michael Hudson <mnkotagu@gmail.com>
2023-03-12 17:25:04 +0300

    Implement 7-add_tuple.py source file

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    len_a = len(tuple_a)
+    len_b = len(tuple_b)
+
+    if len_a < 2:
+        tuple_a = tuple_a + (0,) * (2 - len_a)
+
+    if len_b < 2:
+        tuple_b = tuple_b + (0,) * (2 -len_b)
+
+    sum_x = tuple_a[0] + tuple_b[0]
+    sum_y = tuple_a[1] + tuple_b[1]
+
+    return (sum_x, sum_y)
+
