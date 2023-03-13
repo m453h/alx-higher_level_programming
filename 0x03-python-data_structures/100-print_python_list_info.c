a482e41184775c98b6aec0ea4d2f07131a8cadb2
Michael Hudson <mnkotagu@gmail.com>
2023-03-13 17:24:29 +0300

    Fix syntax errors in 100-print_python_list_info.c source file

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,3 +1,4 @@
+#include <Python.h>
 #include <object.h>
 #include <listobject.h>
 
@@ -11,10 +12,10 @@ void print_python_list_info(PyObject *py)
 	int i;
 	PyListObject *obj = (PyListObject *)py;
 
-	printf("[*] Size of the Python List =%;o\n", size);
-	printf("[*] Allocated =%li\n", obj->allocated);
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
 
 	for (i = 0; i < size; i++)
-		printf("Element %i: %s\n", i Py_TYPE(obj->ob_item[i]->tp_name);
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
 
 }
