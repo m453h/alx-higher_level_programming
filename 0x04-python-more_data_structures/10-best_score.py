524565634ba143afbe7b9e9c65f80c2d9b31ce19
Michael Hudson <mnkotagu@gmail.com>
2023-03-15 19:42:03 +0300

    Improve 10-best_score.py source file

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,15 +2,17 @@
 
 def best_score(a_dictionary):
     max_int = None
-
+    key = ""
     if a_dictionary is None:
         return max_int
 
     for k in a_dictionary:
         if max_int is None:
+            key = k
             max_int = a_dictionary[k]
 
         if a_dictionary[k] > max_int:
+            key = k
             max_int = a_dictionary[k]
 
-    return max_int
+    return key
