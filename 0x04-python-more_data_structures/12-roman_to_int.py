cb851afb91c6f17a6096683a799568386904bd11
Michael Hudson <mnkotagu@gmail.com>
2023-03-15 21:05:51 +0300

    Improve 12-roman_to_int.py source file

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string,str):
+        return 0
+
     conversion_map = {
             'I': 1,
             'V': 5,
