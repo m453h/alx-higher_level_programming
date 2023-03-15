542e35af53682b1d98b0cb74a7f33bd4555a8bec
Michael Hudson <mnkotagu@gmail.com>
2023-03-15 19:06:34 +0300

    Implement 6-print_sorted_dictionary.py source file

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+    keys = list(a_dictionary.keys())
+    keys.sort()
+    for key in keys:
+        value = a_dictionary[key]
+        print(key, ':', value)
