1071177301223525bee4a6021370c35bdd7399df
Michael Hudson <mnkotagu@gmail.com>
2023-04-11 21:32:55 +0300

    Implement 6-base_geometry.py source file

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Define class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Defines a BaseGeometry class instance."""
+
+    def area(self):
+        """
+            Defines generic implemented.
+
+            :raises:
+                    Exception: area() is not implemented.
+
+        """
+        raise Exception("area() is not implemented")
