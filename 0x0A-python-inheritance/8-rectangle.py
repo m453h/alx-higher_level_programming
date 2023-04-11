c146c62634283a7f844e92fe3cb7789bee513641
Michael Hudson <mnkotagu@gmail.com>
2023-04-11 22:19:31 +0300

    Improve implementation of 8-rectangle.py

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,7 +3,7 @@
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
-class Rectangle:
+class Rectangle(BaseGeometry):
     """Defines a Rectangle class instance."""
 
     def __init__(self, width, height):
