9eff0e10371ff4b940ba71a6de452008e279f994
Michael Hudson <mnkotagu@gmail.com>
2023-04-11 22:53:10 +0300

    Implement proper str method for rectangle and square instances

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -25,8 +25,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """ Returns the representation of the Rectangle instance."""
-        return "[{}] {}/{}".format(
-            self.__class__.__name__,
-            self.__width,
-            self.__height
-            )
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
