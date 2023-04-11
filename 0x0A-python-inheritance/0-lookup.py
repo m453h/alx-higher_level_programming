3f144af9239f2b5cc7bb453f558cd8d68fb71180
Michael Hudson <mnkotagu@gmail.com>
2023-04-11 20:18:01 +0300

    Improve 0-lookup.y source file

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" Defines a function returning list of attributes and methods of an object"""
+
+
 def lookup(obj):
     """
         Returns the list of available attributes and methods of an object
