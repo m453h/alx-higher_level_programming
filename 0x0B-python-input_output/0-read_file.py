f65d2e6e4e56e109aae5ed100cedd24cb8a4d401
Michael Hudson <mnkotagu@gmail.com>
2023-04-12 15:30:14 +0300

    Implement 0-read_file.py source file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines a function that reads a text file (UTF8) and prints it to stdout """
+
+
+def read_file(filename=""):
+    """
+        Reads a text file (UTF8) and prints it to stdout
+
+        Args:
+            filename (str): The name of the file to read
+        Return: (None)
+    """
+    with open(filename, "r", encoding="utf-8") as input_file:
+        print(input_file.read(), end="")
---
+++ 0x0B-python-input_output/README.md
@@ -0,0 +1,3 @@
+# 0x0B. Python - Input/Output
+
+This repository contains source code for the Python - Input/Output Project
