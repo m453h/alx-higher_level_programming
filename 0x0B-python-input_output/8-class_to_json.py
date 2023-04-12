c125fe807b7d5b800263f0dc0df9de330eb1f708
Michael Hudson <mnkotagu@gmail.com>
2023-04-12 19:03:30 +0300

    Fix 8-class_to_json.py source file

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -4,7 +4,6 @@ Defines a function that  returns the dictionary description
 with simple data structure (list, dictionary, string, integer and boolean)
 for JSON serialization of an object
 """
-import json
 
 
 def class_to_json(obj):
@@ -21,17 +20,4 @@ def class_to_json(obj):
     if not obj:
         return None
 
-    obj_dictionary = {}
-    attributes = dir(obj)
-
-    for attribute in attributes:
-
-        if callable(getattr(obj, attribute)) or attribute.startswith("__"):
-            continue
-            
-        attribute_value = getattr(obj, attribute)
-       
-        if isinstance(attribute_value, (list, dict, str, int, bool)):
-            obj_dictionary[attribute] = attribute_value
-
-    return obj_dictionary
+    return obj.__dict__
