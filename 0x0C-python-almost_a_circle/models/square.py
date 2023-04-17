73117868d10c9d4311487ac795eec803e08f9486
Michael Hudson <mnkotagu@gmail.com>
2023-04-17 11:58:27 +0300

    Implement update method for the Square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -19,11 +19,10 @@ class Square(Rectangle):
 
     def __str__(self):
         """Returns the representation of the Square instance"""
-        return "[Square] ({}) {}/{} - {}/{}".format(self.id,
+        return "[Square] ({}) {}/{} - {}".format(self.id,
                                                     self.x,
                                                     self.y,
-                                                    self.width,
-                                                    self.height)
+                                                    self.width)
 
     @property
     def size(self):
@@ -44,3 +43,40 @@ class Square(Rectangle):
         """
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """
+        Assigns arguments passed to the program to the Square instance
+
+        Args:
+            *args:  Variable number of arguments, expected to contain integers
+                    where:
+                        1st argument is the id attribute
+                        2nd argument is the size attribute
+                        3rd argument is the x attribute
+                        4th argument is the y attribute
+
+            **kwargs: Variable number of Key/value arguments
+        """
+        if args and len(args) != 0:
+            iteration = 0
+            for arg in args:
+                if iteration == 0:
+                    self.id = arg
+                elif iteration == 1:
+                    self.size = arg
+                elif iteration == 2:
+                    self.x = arg
+                elif iteration == 3:
+                    self.y = arg
+                iteration += 1
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                elif key == "size":
+                    self.size = value
+                elif key == "x":
+                    self.x = value
+                elif key == "y":
+                    self.y = value
