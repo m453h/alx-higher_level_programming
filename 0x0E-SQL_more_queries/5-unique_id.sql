764ae82f6c3d85fd48655e110766ad6fff6aab3e
Michael Hudson <mnkotagu@gmail.com>
2023-05-17 20:38:49 +0300

    Implement 5-unique_id.sql and 6-states.sql

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,6 @@
+-- This script creates the table unique_id with:
+-- i) id INT with the default value 1 and must be unique
+-- ii) name VARCHAR(256)
+-- If the table unique_id already exists, your script should not fail
+
+CREATE TABLE IF NOT EXISTS `unique_id` (`id` INT DEFAULT 1 UNIQUE, `name` VARCHAR(256));
