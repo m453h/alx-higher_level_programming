baaf25b486924a3ce59df4a8a594bc786c895c37
Michael Hudson <mnkotagu@gmail.com>
2023-05-17 21:00:37 +0300

    Implement 10-genre_id_by_show.sql

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- This script lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
+-- Each record should display: tv_shows.title - tv_show_genres.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- You can use only one SELECT statement
+-- The database name will be passed as an argument of the mysql command
+
+SELECT ts.`title`, tsg.`genre_id`
+FROM `tv_shows` AS ts
+INNER JOIN `tv_show_genres` AS tsg ON ts.`id` = tsg.`show_id`
+ORDER BY ts.`title`, tsg.`genre_id`;
