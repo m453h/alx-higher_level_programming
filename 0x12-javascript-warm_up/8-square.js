cdc5afb50e238498a67cf4f11f76de71e519b9ac
Michael Hudson <mnkotagu@gmail.com>
2023-06-12 18:52:33 +0300

    Implement 8-square.js

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const x = parseInt(process.argv[2]);
+
+if (!isNaN(x)) {
+  for (let i = 0; i < x; i++) {
+    for (let j = 0; j < x; j++)
+    {
+      process.stdout.write('X');
+    }
+
+    process.stdout.write('\n');
+  }
+} else {
+  console.log('Missing size');
+}
