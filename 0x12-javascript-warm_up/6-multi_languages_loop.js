1a795092887277adb3165c39b15251861141401a
Michael Hudson <mnkotagu@gmail.com>
2023-06-12 18:36:37 +0300

    Fix output of 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const languages = ['C', 'Python', 'JavaScript'];
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
 for (const language of languages) {
-  console.log(language + ' is fun');
+  console.log(language);
 }
