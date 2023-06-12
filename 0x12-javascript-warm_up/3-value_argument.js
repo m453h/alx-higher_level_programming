01ee924f176f7049d6ba153eecdd7c53a95f191a
Michael Hudson <mnkotagu@gmail.com>
2023-06-12 17:43:12 +0300

    Remove length function

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const argsLength = process.argv.length - 2;
 
-if (argsLength < 3 || typeof process.argv[2] === 'undefined') {
+if (typeof process.argv[2] === 'undefined') {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
