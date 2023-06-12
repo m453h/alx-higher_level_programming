3dde5199f7829c6eadcaaa9f1ef126025b599477
Michael Hudson <mnkotagu@gmail.com>
2023-06-12 17:48:13 +0300

    Remove args count check

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,8 +1,2 @@
 #!/usr/bin/node
-
-if (typeof process.argv[2] === 'undefined' ||
-  typeof process.argv[3] === 'undefined') {
-  console.log('No argument');
-} else {
-  console.log(process.argv[2] + ' is ' + process.argv[3]);
-}
+console.log(process.argv[2] + ' is ' + process.argv[3]);
