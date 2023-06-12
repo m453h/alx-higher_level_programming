fc03e586c1383dd48409cbedd43f7f7efcde2e2b
Michael Hudson <mnkotagu@gmail.com>
2023-06-12 19:58:32 +0300

    Improve 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,10 @@ const argsLength = process.argv.length - 2;
 if (argsLength === 0 || argsLength === 1) {
   console.log(0);
 } else {
-  let largest = process.argv[2];
-  let secondLargest = process.argv[3];
+  let largest = parseInt(process.argv[2]);
+  let secondLargest = parseInt(process.argv[3]);
   for (let i = 1; i < argsLength; i++) {
-    const currentValue = process.argv[i + 2];
+    const currentValue = parseInt(process.argv[i + 2]);
     if (largest < currentValue) {
       secondLargest = largest;
       largest = currentValue;
