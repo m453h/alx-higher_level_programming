7e75784bc0e64cad95d00e3eabd3b6913ed8b42c
Michael Hudson <mnkotagu@gmail.com>
2023-06-12 17:36:18 +0300

    Improve 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
+const argsLength = process.argv.length - 2;
 const args = process.argv;
 
-if (args[2] === 'undefined') {
+if (argsLength < 1 || typeof args[2] === 'undefined') {
   console.log('No argument');
 } else {
   console.log(args[2]);
