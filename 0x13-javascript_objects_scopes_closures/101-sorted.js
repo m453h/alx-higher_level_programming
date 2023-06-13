e7fc8d02cf5976af8d54fe396f11741426d9f427
Michael Hudson <mnkotagu@gmail.com>
2023-06-13 22:01:10 +0300

    Implement 101-sorted.js

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const { dict } = require('./101-data.js');
+const newDict = {};
+for (const userId in dict) {
+  const occurrence = dict[userId];
+
+  if (!newDict[occurrence]) {
+    newDict[occurrence] = [userId];
+  } else {
+    newDict[occurrence].push(userId);
+  }
+}
+console.log(newDict);
