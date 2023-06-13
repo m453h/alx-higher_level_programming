14d0ee75b0adea32c5c8d51068019052a45c41d7
Michael Hudson <mnkotagu@gmail.com>
2023-06-13 21:39:42 +0300

    Improve 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reversedList = [];
+  const reversedList = [];
   for (let i = list.length - 1; i > -1; i--) {
     reversedList.push(list[i]);
   }
