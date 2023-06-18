c5a71b86c9e555b7f6355bd0fc2c5e3a2db4aa1c
Michael Hudson <mnkotagu@gmail.com>
2023-06-18 13:21:38 +0300

    Implement 10-model_state_my_get.py

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import State
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
+                           .format(
+                            sys.argv[1],
+                            sys.argv[2],
+                            sys.argv[3]
+                            ),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State)\
+        .filter(State.name == sys.argv[4])\
+        .order_by(State.id)\
+        .first()
+    if state is not None:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
