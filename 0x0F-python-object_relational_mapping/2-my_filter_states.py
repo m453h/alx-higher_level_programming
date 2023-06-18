72f036420c4029183874f26678156c2792a78cd4
Michael Hudson <mnkotagu@gmail.com>
2023-06-18 08:50:34 +0300

    Implement 2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the last argument passed.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` where name LIKE '{:s}'\
+     ORDER BY `id` ASC".format(sys.argv[4]))
+    states = cursor.fetchall()
+    for state in states:
+        print(state)
