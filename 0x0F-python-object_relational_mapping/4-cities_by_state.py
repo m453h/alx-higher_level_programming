4b8a5f973c41437122103a92fb0b90fc07c39ec6
Michael Hudson <mnkotagu@gmail.com>
2023-06-18 11:19:16 +0300

    Implement 4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Displays all values in the states table of hbtn_0e_0_usa
+where name matches the last argument passed.
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+    cursor = db.cursor()
+    query = "SELECT `c`.`id`, `c`.`name`, `s`.`name`\
+     FROM `cities` as `c` INNER JOIN `states` as `s`\
+      ON `c`.`state_id` = `s`.`id`\
+       ORDER BY `c`.`id` ASC"
+    cursor.execute(query)
+    cities = cursor.fetchall()
+    for city in cities:
+        print(city)
