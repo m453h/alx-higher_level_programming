1cd0c5a0838c447036370c810d53e7a8f6c54a9b
Michael Hudson <mnkotagu@gmail.com>
2023-06-18 08:41:58 +0300

    Add sort in ascending order

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         db=sys.argv[3]
         )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states`")
+    cursor.execute("SELECT * FROM `states` ORDER BY `id` ASC")
     states = cursor.fetchall()
     for state in states:
         print(state)
