9e1bba78858b7e57e67015205463c402c9fbc7f3
Michael Hudson <mnkotagu@gmail.com>
2023-07-01 01:01:21 +0300

    Improve 102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Makes a request to 0.0.0.0:5000/catch_me
-curl -o /dev/null -sw "You got me!" -sL -X PUT 0.0.0.0:5000/catch_me
+curl -o /dev/null -w "You got me!" -sL -X PUT 0.0.0.0:5000/catch_me
