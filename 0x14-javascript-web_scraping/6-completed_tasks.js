88c3aa6e77ed10a63c4287452c4832a379efd98e
Michael Hudson <mnkotagu@gmail.com>
2023-07-25 22:56:10 +0300

    Attempt to improve 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,20 +3,26 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response, body) {
+request.get(url, { json: true }, (error, response, body) => {
   if (error) {
     console.log(error);
-  } else {
-    if (response.statusCode === 200) {
-        const todos = JSON.parse(body);
-        const data = {};
-        for (const todo of todos) {
-          if (todo.completed == true) {
-            const userId = todo.userId.toString();
-            data[userId] = (data[userId] || 0) + 1;
-          }
-        }
-        console.log(data);
-    }
+    return;
   }
+
+  if (response.statusCode !== 200) {
+    console.error('Error Code:', response.statusCode);
+    return;
+  }
+
+  const data = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (data[todo.userId] === undefined) {
+        data[todo.userId] = 1;
+      } else {
+        data[todo.userId]++;
+      }
+    }
+  });
+  console.log(data);
 });
