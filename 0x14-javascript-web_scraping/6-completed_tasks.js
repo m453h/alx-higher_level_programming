13eee38d24e52fdf05d41906119040ee316ed57a
Michael Hudson <mnkotagu@gmail.com>
2023-07-25 18:07:47 +0300

    Attempts alternative solution

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,39 +3,24 @@
 const request = require('request');
 const url = process.argv[2];
 
-request(url, function (error, response) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
   } else {
     if (response.statusCode === 200) {
-      const data = {};
       try {
-        const todos = JSON.parse(response.body);
-
-        if (typeof todos[Symbol.iterator] === 'function') {
-          for (const todo of todos) {
-            const userId = todo.userId;
-            const status = todo.completed;
-            if (status) {
-              if (!(userId in data)) {
-                data[userId] = 1;
-              } else {
-                data[userId] = data[userId] + 1;
-              }
-            }
-          }
-        } else {
-          if (todos.completed) {
-            data[todos.userId] = 1;
+        const todos = JSON.parse(body);
+        const data = {};
+        for (const todo of todos) {
+          if (todo.completed) {
+            const userId = todo.userId.toString();
+            data[userId] = (data[userId] || 0) + 1;
           }
         }
+        console.log(data);
       } catch (parseError) {
         console.error(parseError);
       }
-
-      console.log(data);
-    } else {
-      console.log(response.statusCode);
     }
   }
 });
