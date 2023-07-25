38f06e3e19e397a3a8a1127bb5139ce4ce04d379
Michael Hudson <mnkotagu@gmail.com>
2023-07-25 15:11:31 +0300

    Improve 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-const file_path = process.argv[2];
+const filePath = process.argv[2];
 
-fs.readFile(file_path, 'utf-8', function (err, content) {
+fs.readFile(filePath, 'utf-8', function (err, content) {
   if (err) {
     console.log(err);
   } else {
