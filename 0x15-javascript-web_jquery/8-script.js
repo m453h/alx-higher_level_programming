040b47cb334138133c436fbaee942ea7e4b9b540
Michael Hudson <mnkotagu@gmail.com>
2023-08-02 16:51:03 +0300

    Implement 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,5 @@
+$.getJSON('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+  for (let i = 0; i < data.results.length; i++) {
+    $('ul#list_movies').append('<li>' + data.results[i].title + '</li>');
+  }
+});
